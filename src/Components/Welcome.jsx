72bcd71344908dab38263848498731ac4ab2b11a
Luis <luisfelipe.sb@outlook.com>
2022-09-09 13:48:10 -0300

    teste

---
+++ src/Components/Welcome.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Welcome() {
+  return (
+    <div>
+      <h1>Meu nome é Luis, seja bem vindo ao sistema</h1>
+      <p>😀</p>
+    </div>
+  )
+}
+
+export default Welcome
